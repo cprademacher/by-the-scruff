a4416837c1a5751e6943cb076629583385ecca1b
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-02-17 23:34:46 -0600

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv";
+const app = express();
+import { connectDatabase } from "./config/dbConnect.js";
+
+dotenv.config({ path: "server/config/config.env" });
+
+// Connecting to database
+connectDatabase();
+
+// Import all routes
+import productRoutes from "./routes/products.js";
+
+app.use("/api", productRoutes);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server started on PORT: ${process.env.PORT} in ${process.env.NODE_ENV} mode.`
+  );
+});
