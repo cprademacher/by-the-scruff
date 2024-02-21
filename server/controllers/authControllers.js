e80cc96a2508000d72c595d55c03e04d2076b9ac
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-02-21 17:17:02 -0600

    Added nodemailer and mailtrap to be able to allow users to reset their password and when they click on a button they get auto-emailed a reset password email

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,6 +1,8 @@
 import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 import User from "../models/User.js";
+import { getResetPasswordTemplate } from "../utils/emailTemplates.js";
 import ErrorHandler from "../utils/errorHandler.js";
+import sendEmail from "../utils/sendEmail.js";
 import sendToken from "../utils/sendToken.js";
 
 // Register user => /api/register
@@ -52,4 +54,44 @@ export const logoutUser = catchAsyncErrors(async (req, res, next) => {
   res.status(200).json({
     message: "Logged Out",
   });
+});
+
+// Forgot Password => /api/password/forgot
+export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
+  //   Find user in the database
+  const user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new ErrorHandler("User not found with this email", 404));
+  }
+
+  //   Get reset password token
+  const resetToken = user.getResetPasswordToken();
+
+  await user.save();
+
+  // Create password reset url
+  const resetUrl = `${process.env.FRONTEND_URL}/api/password/reset/${resetToken}`;
+
+  const message = getResetPasswordTemplate(user?.name, resetUrl);
+
+  try {
+    await sendEmail({
+      email: user.email,
+      subject: "Driftwood Clothing Co. Password Recovery",
+      message,
+    });
+
+    res.status(200).json({
+      message: `Email sent to: ${user.email}`,
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+
+    await user.save();
+    return next(new ErrorHandler(error?.message, 500));
+  }
+
+  sendToken(user, 200, res);
 });
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,11 +3,14 @@ import {
   registerUser,
   loginUser,
   logoutUser,
+  forgotPassword,
 } from "../controllers/authControllers.js";
 const router = express.Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(logoutUser);
+
+router.route("/password/forgot").post(forgotPassword);
 
 export default router;
