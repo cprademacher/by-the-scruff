c77a6e8a45e5c4b6e10ef26e6670b9d351374717
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-02-23 10:39:50 -0600

    Added a route to access all the current users orders as well as the user info to post in the browser along with their orders

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -31,3 +31,33 @@ export const newOrder = catchAsyncErrors(async (req, res, next) => {
     order,
   });
 });
+
+// Get current user oders  => /api/me/orders/
+export const myOrders = catchAsyncErrors(async (req, res, next) => {
+  // 'populate("user")' below comes form mongoose and allows us to see all the user data in the returned orders data
+  const orders = await Order.find({ user: req.user._id }).populate(
+    "user",
+    "name email"
+  );
+
+  if (!orders) {
+    return next(new ErrorHandler("No order found with this ID.", 404));
+  }
+
+  res.status(200).json({
+    orders,
+  });
+});
+
+// Get Order Details => /api/orders/:id
+export const getOrderDetails = catchAsyncErrors(async (req, res, next) => {
+  const order = await Order.findById(req.params.id);
+
+  if (!order) {
+    return next(new ErrorHandler("No order found with this ID.", 404));
+  }
+
+  res.status(200).json({
+    order,
+  });
+});
--- server/routes/order.js
+++ server/routes/order.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { newOrder } from "../controllers/orderControllers.js";
+import {
+  newOrder,
+  getOrderDetails,
+  myOrders,
+} from "../controllers/orderControllers.js";
 import { isAuthenticatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
 router.route("/orders/new").post(isAuthenticatedUser, newOrder);
+router.route("/orders/:id").get(isAuthenticatedUser, getOrderDetails);
+router.route("/me/orders/").get(isAuthenticatedUser, myOrders);
 
 export default router;
