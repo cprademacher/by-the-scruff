d65ee257f31b6fc0b4f6696a644d1f302f59ee50
Cody Rademacher <codyrademacher@Codys-MBP.lan>
2024-02-27 11:00:17 -0600

    Created an api to fetch all products with redux toolkit createApi and fetchBaseQuery

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { productApi } from "./api/productsApi";
 
 // eslint-disable-next-line no-unused-vars
-const store = configureStore({ reducer: {} });
+export const store = configureStore({
+  reducer: {
+    [productApi.reducerPath]: productApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productApi.middleware),
+});
 // The store now has redux-thunk added and the Redux DevTools Extension is turned on
-
-export default store;
