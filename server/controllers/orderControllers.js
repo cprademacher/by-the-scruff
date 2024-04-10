000f5a2d4ea779537631118b27a22ce8aa854802
Cody Rademacher <cprademacher36@gmail.com>
2024-04-10 12:27:30 -0500

    Added ability to save sales and order data between selected dates

--- server/controllers/orderControllers.js
+++ server/controllers/orderControllers.js
@@ -142,10 +142,50 @@ async function getSalesData(startDate, endDate) {
           date: { $dateToString: { format: "%Y-%m-%d", date: "$createdAt" } },
         },
         totalSales: { $sum: "$totalAmount" },
-        numOrder: { $sum: 1 }, // count the number of orders
+        numOrders: { $sum: 1 }, // count the number of orders
       },
     },
   ]);
+
+  // Create a map to store sales data and num of order data
+  const salesMap = new Map();
+  let totalSales = 0;
+  let totalNumOrders = 0;
+
+  salesData.forEach((entry) => {
+    const date = entry?._id.date;
+    const sales = entry?.totalSales;
+    const numOrders = entry?.numOrders;
+
+    salesMap.set(date, { sales, numOrders });
+    totalSales += sales;
+    totalNumOrders += numOrders;
+  });
+
+  // Generate an array of dates between start and end date
+  const datesBetween = getDatesBetween(startDate, endDate);
+
+  // Create final sales data array with 0 for dates without sales
+  const finalSalesData = datesBetween.map((date) => ({
+    date,
+    sales: (salesMap.get(date) || { sales: 0 }).sales,
+    numOrders: (salesMap.get(date) || { numOrders: 0 }).numOrders,
+  }));
+
+  return { salesData: finalSalesData, totalSales, totalNumOrders };
+}
+
+function getDatesBetween(startDate, endDate) {
+  const dates = [];
+  let currentDate = new Date(startDate);
+
+  while (currentDate <= new Date(endDate)) {
+    const formattedDate = currentDate.toISOString().split("T")[0];
+    dates.push(formattedDate);
+    currentDate.setDate(currentDate.getDate() + 1);
+  }
+
+  return dates;
 }
 
 // Get Sales Data - ADMIN => /api/admin/get_sales
@@ -156,9 +196,14 @@ export const getSales = catchAsyncErrors(async (req, res, next) => {
   startDate.setUTCHours(0, 0, 0, 0);
   endDate.setUTCHours(23, 59, 59, 999);
 
-  getSalesData();
+  const { salesData, totalSales, totalNumOrders } = await getSalesData(
+    startDate,
+    endDate
+  );
 
   res.status(200).json({
-    success: true,
+    totalSales,
+    totalNumOrders,
+    sales: salesData,
   });
 });
